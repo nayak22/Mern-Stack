2fadd3eb1c7235a58d70ff31a2591c51ca108e3a
Nayak22 <Nayaksamrath2202@gmail.com>
2023-07-20 18:15:41 +0530

    Committing array tutorial

--- JS Basics 03/index.js
+++ JS Basics 03/index.js
@@ -78,3 +78,126 @@ console.log(date.getMonth());
 console.log(date.getUTCDate());
 console.log(date.getHours());
 console.log(date.getUTCHours());
+
+//Arrays
+let numbers = [1, 4, 5, 7];
+console.log(numbers);
+
+//to add element in the end
+numbers.push(9);
+console.log(numbers);
+
+//to add in the begining
+numbers.unshift(8);
+console.log(numbers);
+
+//To add item in the middle
+numbers.splice(2, 0, "a", "b", "c", "d");
+console.log(numbers);
+
+//to find the element
+console.log(numbers.indexOf("a")); //2
+console.log(numbers.indexOf("i")); //-1 when element is not found
+
+console.log(numbers.includes(7)); //return true or false which can be used for conditioninal checks
+console.log(numbers.indexOf(0, 3)); //return -1 as it check after index 2 and 0 is present at 1
+
+//includes and index of on reference type
+let courses = [
+  { no: 1, firstName: "Love" },
+  { no: 2, firstName: "Rahul" },
+];
+console.log(courses.includes({ no: 1, firstName: "Love" })); //return  false because here parameter object and object in teh array has different reference value
+console.log(courses.indexOf({ no: 1, firstName: "Love" })); //return -1 as  here parameter object and object in teh array has different reference value
+
+//finding the object in the array using the predicate fuction
+
+let course = courses.find(function (course) {
+  return course.firstName === "Love"; //if present it will return the object else will return undefined
+});
+console.log(course);
+
+//Removing the elemnt from the array
+
+let numbers2 = [1, 2, 3, 4, 5, 6, 7];
+console.log(numbers2);
+
+//removing at the end
+numbers2.pop();
+console.log(numbers2);
+
+//removing in the starting
+numbers2.shift();
+console.log(numbers2);
+
+//removing in the middle
+numbers2.splice(2, 1);
+console.log(numbers2);
+
+// Emptying the array
+let number3 = [1, 2, 3, 4, 5, 6, 7];
+let number4 = number3; //here reference is copied
+
+//1st way- Empty array
+//number3=[];// assign to empty array but here number 4 will not be empty and this will get new reference old reference is not deleted
+
+//2nd way-Set let 0 by doing so reference will be cleaned or emptied but will be empty
+//number3.length=0;
+
+//3rd way-Using slice ,method
+number3.slice(0, number3.length);
+
+console.log(number3, number4);
+
+//Combining and slicing the arrays
+let first = [1, 2, 3];
+let second = [4, 5, 6];
+
+let combined = first.concat(second);
+console.log(combined);
+
+//slicing
+let marks = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+let sliced = marks.slice(3, 6); //3rd position inclusive and 6th position exclusive
+
+console.log(sliced);
+
+//spread operator
+
+let combined2 = [...first, ...second];
+console.log(combined2);
+
+//can  add additional elements too
+
+let combined3 = [...first, "a", ...second, "c"];
+console.log(combined3);
+
+//we can use this to create copy too
+let combined3Copy = [...combined3];
+console.log(combined3Copy);
+
+let arr = [10, 20, 30, 40, 50];
+
+//using for of loop
+for (let value of arr) {
+  console.log(value);
+}
+
+//using for each and call back.
+arr.forEach(function (number) {
+  console.log(number);
+});
+
+//samething can be written using arrow function
+arr.forEach((number) => console.log(number));
+
+//joining the array
+let numberList = [10, 20, 30, 40, 50];
+let joined = numberList.join(",");
+console.log(joined);
+console.log(typeof joined);
+
+//splittng to form the array
+let message2 = "Hi hello namskar";
+let combinedArr = message2.split(" ");
+console.log(combinedArr);
